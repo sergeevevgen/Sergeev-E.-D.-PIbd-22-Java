10fea13aedb7d1e9533ec597f9262bb1fc9bb4da
Евгений Сергеев <alexevgenii@mail.ru>
2021-10-03 14:32:15 +0400

    Add button

--- Labs/src/FormMonoRels.java
+++ Labs/src/FormMonoRels.java
@@ -1,5 +1,19 @@
 import javax.swing.*;
 
-public class FormMonoRels {
+public class FormMonoRels extends JFrame{
     private JPanel PanelMain;
+    private JButton buttonMain;
+
+    public FormMonoRels()
+    {
+        setContentPane(PanelMain);
+        setVisible(true);
+        setSize(640,480);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args)
+    {
+        new FormMonoRels();
+    }
 }
